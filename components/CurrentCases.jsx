771d636593b1e024671e08b2d1cdb33a66f1ad2d
muhammadfarooq897123 <100750246+muhammadfarooq897123@users.noreply.github.com>
2025-03-24 00:14:34 +0500

    Update CurrentCases.jsx

--- components/CurrentCases.jsx
+++ components/CurrentCases.jsx
@@ -23,19 +23,7 @@ export default function CurrentCases() {
          "Account3": "00300110953386 (Abdul Rehman)  Meezan Bank Account",
          "contact": "03089431073 (Anas Naveed President)"
       },
-      {
-
-         "title": "Medical Camp case",
-         "description": "Serve Humanity in collaboration with Kaar-e-Kamal Taxila Chapter is conducting it's very first medical camp on 15th of February 2025 at Rehbar Colony. To make this Kaar e Khair successful we need your support so we humbly request everyone to participate in making this happen so we can reach out to those in need",
-         "AmountRequired": "50k",
-         "AmountCollected": "10k",
-         "Deadline": "15 feb",
-         "Account1": "03196092861   (Abdul Rehman ) jazzcash",
-         "Account2": "03006844332 (Muhammad Junaid Rashid) Easypaisa",
-         "Account3": "00300110953386 (Abdul Rehman)  Meezan Bank Account",
-         "contact": "+923049790694"
-
-      },
+      
    ]
 
    const getDocuments = (case_val, case_contact)=>{
